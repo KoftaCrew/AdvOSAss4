a755878384461894b25e3d21f86751ae4d3eb6b3
Kareem Morsy <kreem.morsy@hotmail.com>
2022-05-19 23:56:26 +0200

    Simulator functions

--- src/Directory.java
+++ src/Directory.java
@@ -1,9 +1,61 @@
 import java.util.List;
 
 public class Directory {
-    private String name;
+    private final String name;
     private List<Directory> directories;
     private List<File> files;
 
+    public Directory(String name) {
+        this.name = name;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public List<Directory> getDirectories() {
+        return directories;
+    }
+
+    public List<File> getFiles() {
+        return files;
+    }
+
+    public void addFile(File file) {
+        files.add(file);
+    }
+
+    public void removeFile(File file) {
+        files.remove(file);
+    }
+
+    public void addDirectory(Directory directory) {
+        directories.add(directory);
+    }
+
+    public void removeDirectory(Directory directory) {
+        directories.remove(directory);
+    }
+
+    public Directory getDirectory(String name) {
+        for (Directory d :
+                directories) {
+            if (d.name.equalsIgnoreCase(name)) {
+                return d;
+            }
+        }
+
+        return null;
+    }
+
+    public File getFile(String name) {
+        for (File f :
+                files) {
+            if (f.getName().equalsIgnoreCase(name)) {
+                return f;
+            }
+        }
+
+        return null;
+    }
 }
--- src/File.java
+++ src/File.java
@@ -1,4 +1,17 @@
 public class File {
-    String name;
-    int size;
+    private final String name;
+    private final int size;
+
+    public File(String name, int size) {
+        this.name = name;
+        this.size = size;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSize() {
+        return size;
+    }
 }
